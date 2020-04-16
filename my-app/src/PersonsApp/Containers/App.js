3a4da7a080e38c4ed474b3130411333077d435fb
Rodrigo Soria <rodsilver83@hotmail.com>
2020-04-16 11:41:49 -0400

    state update on prevState

--- my-app/src/PersonsApp/Containers/App.js
+++ my-app/src/PersonsApp/Containers/App.js
@@ -15,7 +15,8 @@ class App extends Component {
         { id: 'tbie', name: 'Marco', age: 25 },
       ],
       showPersons: false,
-      showCockpit: true
+      showCockpit: true,
+      changeCounter: 0
     }
   }
 
@@ -37,8 +38,11 @@ class App extends Component {
 
     const persons = [...this.state.persons];
     persons[index] = person;
-    this.setState({
-      persons: persons
+    this.setState((prevState, props) => {
+      return {
+        persons: persons,
+        changeCounter: prevState.changeCounter + 1
+      }
     });
   }
 
