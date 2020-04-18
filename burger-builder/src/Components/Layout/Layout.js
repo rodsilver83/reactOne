022dbacdcf9d88feab6471689b7d11d783bed880
Rodrigo Soria <rodsilver83@hotmail.com>
2020-04-18 01:01:57 -0400

    change logo

--- burger-builder/src/Components/Layout/Layout.js
+++ burger-builder/src/Components/Layout/Layout.js
@@ -1,17 +1,33 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Aux from '../../Hoc/Aux';
 import ToolBar from '../Navigation/Toolbar/Toolbar';
 import css from './Layout.module.css';
 import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
-const Layout = (props) => (
-  <Aux>
-    <ToolBar />
-    <SideDrawer />
-    <main className={css.Content}>
-      {props.children}
-    </main>
-  </Aux>
-);
+class Layout extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      showSideDrawer: true
+    }
+  }
 
+  sideDrawerClosedHandler = () => {
+    this.setState({ showSideDrawer: false });
+  }
+
+  render() {
+    return (
+      <Aux>
+        <ToolBar />
+        <SideDrawer
+          closed={this.sideDrawerClosedHandler}
+          open={this.state.showSideDrawer} />
+        <main className={css.Content}>
+          {this.props.children}
+        </main>
+      </Aux>
+    );
+  }
+}
 export default Layout;
--- burger-builder/src/Components/Navigation/SideDrawer/SideDrawer.js
+++ burger-builder/src/Components/Navigation/SideDrawer/SideDrawer.js
@@ -2,18 +2,31 @@ import React from 'react';
 import Logo from '../../Logo/Logo';
 import NavigationItems from '../NavigationItems/Navigationitems';
 import css from './SideDrawer.module.css';
+import Backdrop from '../../UI/Backdrop/Backdrop';
+import Aux from '../../../Hoc/Aux';
 
 const SideDrawer = (props) => {
-  // ...
+  const sideDrawerClasses = [
+    css.SideDrawer
+  ];
+  if (props.open) {
+    sideDrawerClasses.push(css.Open);
+  } else {
+    sideDrawerClasses.push(css.Close);
+  }
+
   return (
-    <div className={css.SideDrawer}>
-      <div className={css.Logo}>
-        <Logo />
+    <Aux>
+      <Backdrop show={props.open} clicked={props.closed} />
+      <div className={sideDrawerClasses.join(' ')}>
+        <div className={css.Logo}>
+          <Logo />
+        </div>
+        <nav>
+          <NavigationItems />
+        </nav>
       </div>
-      <nav>
-        <NavigationItems />
-      </nav>
-    </div>
+    </Aux>
   );
 }
 
