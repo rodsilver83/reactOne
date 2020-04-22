2713123dadc8dde80578452227808cca31deebf5
Rodrigo Soria <rodsilver83@hotmail.com>
2020-04-22 10:58:35 -0400

    Toal price style

--- burger-builder/src/Components/Burger/BuildControls/BuildControls.js
+++ burger-builder/src/Components/Burger/BuildControls/BuildControls.js
@@ -13,7 +13,7 @@ const controls = [
 
 const buildControls = (props) => (
   <div className={css.BuildControls}>
-    <p>Current Price: <strong>${props.price.toFixed(2)}</strong></p>
+    <p className={css.Price}>Current Price: <strong>${props.price.toFixed(2)}</strong></p>
     {controls.map(control => (
       <BuildControl
         key={control.label}
