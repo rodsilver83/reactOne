8e97b5297a16c6c993a035153342965d1f81656a
Rodrigo Soria <rodsilver83@hotmail.com>
2020-04-26 00:42:46 -0400

    build to docs folder

---
+++ docs/precache-manifest.3feb5ae31e1ae8e1aab2ebd321c306aa.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "438e8b553622cb34bf629da9eea77284",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7c5d6cda0b84c069450f",
+    "url": "/static/css/main.722a3b84.chunk.css"
+  },
+  {
+    "revision": "8d0c77d5a430e21df548",
+    "url": "/static/js/2.505309e6.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.505309e6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7c5d6cda0b84c069450f",
+    "url": "/static/js/main.c13bd337.chunk.js"
+  },
+  {
+    "revision": "771db5c0ceae6ff6d487",
+    "url": "/static/js/runtime-main.b48f82dd.js"
+  },
+  {
+    "revision": "3f37c71e6f3d0c6e3959eccfae699c9b",
+    "url": "/static/media/burgerIcon.3f37c71e.png"
+  }
+]);
